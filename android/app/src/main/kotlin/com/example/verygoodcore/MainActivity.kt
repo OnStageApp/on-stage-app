2c2bcf668e807be34b49188036f4ef34e6374ff6
ToniVinter <tonyivinter@gmail.com>
2023-06-22 23:34:57 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.on_stage_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:on_stage_app/app/app.dart';
+import 'package:on_stage_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
