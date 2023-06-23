21fc27623f335bdbd9d10e42d80ac29cafe4f1e8
ToniVinter <tonyivinter@gmail.com>
2023-06-23 13:03:20 +0300

    fix: workflows updated 5

--- test/app/initial_test.dart
+++ test/app/initial_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:on_stage_app/app/app.dart';
+import 'package:on_stage_app/counter/view/counter_page.dart';
 
 void main() {
-  group(
-    'Activity selection page tests',
-    () {},
-  );
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
 }
