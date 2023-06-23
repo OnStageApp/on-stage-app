602d7ddc8d7b90882ac7e73c33f114fa8ae8b945
ToniVinter <tonyivinter@gmail.com>
2023-06-23 13:13:58 +0300

    fix: workflows updated 5

--- test/app/initial_test.dart
+++ test/app/initial_test.dart
@@ -1,8 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:on_stage_app/app/app.dart';
+
 void main() {
-  // group('App', () {
-  //   testWidgets('renders CounterPage', (tester) async {
-  //     await tester.pumpWidget(const App());
-  //     expect(find.byType(CounterPage), findsOneWidget);
-  //   });
-  // });
+  group('App', () {
+    testWidgets('renders App', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(App), findsOneWidget);
+    });
+  });
 }
