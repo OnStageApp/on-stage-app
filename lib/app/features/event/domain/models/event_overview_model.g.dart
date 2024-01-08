ee6203c69ff75ce25e4e1b5d2b235fdefde98b94
ToniVinter <tonyivinter@gmail.com>
2024-01-08 15:43:09 +0200

    generated files

---
+++ lib/app/features/event/domain/models/event_overview_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'event_overview_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$EventOverviewImpl _$$EventOverviewImplFromJson(Map<String, dynamic> json) =>
+    _$EventOverviewImpl(
+      id: json['id'] as String,
+      title: json['title'] as String,
+      date: json['date'] as String,
+    );
+
+Map<String, dynamic> _$$EventOverviewImplToJson(_$EventOverviewImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'date': instance.date,
+    };
