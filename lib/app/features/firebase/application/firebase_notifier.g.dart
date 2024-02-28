7a4e6ee2ff83de0e634ea6b8733fdfa9e4a0a803
emanuel <manu.farcau@gmail.com>
2024-02-28 15:33:43 +0200

    Setup for pushnotifications

---
+++ lib/app/features/firebase/application/firebase_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'firebase_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseNotifierHash() => r'2b125dea73b5108d3c9d395bcc602bf66f078ce1';
+
+/// See also [FirebaseNotifier].
+@ProviderFor(FirebaseNotifier)
+final firebaseNotifierProvider =
+    NotifierProvider<FirebaseNotifier, FirebaseState>.internal(
+  FirebaseNotifier.new,
+  name: r'firebaseNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$firebaseNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$FirebaseNotifier = Notifier<FirebaseState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/app/features/firebase/application/firebase_state.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+
+class FirebaseState extends Equatable{
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+}
