c5da31dc686a7a71a3465ed501656e3050240520
ToniVinter <tonyivinter@gmail.com>
2024-10-06 03:27:29 +0300

    First run on android

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:on_stage_app/app/app.dart';
 import 'package:on_stage_app/bootstrap.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // await FirebaseNotifier().init();
 
