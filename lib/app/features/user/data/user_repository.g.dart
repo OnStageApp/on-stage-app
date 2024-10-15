3b576c5a580ec3606bdba2b53cbd5c75932a547c
ToniVinter <tonyivinter@gmail.com>
2024-10-15 22:35:05 +0300

    notification changes

--- lib/app/features/user/data/user_repository.g.dart
+++ lib/app/features/user/data/user_repository.g.dart
@@ -24,12 +24,12 @@ class _UserRepository implements UserRepository {
   final ParseErrorLogger? errorLogger;
 
   @override
-  Future<List<User>> getUsers() async {
+  Future<List<UserModel>> getUsers() async {
     final _extra = <String, dynamic>{};
     final queryParameters = <String, dynamic>{};
     final _headers = <String, dynamic>{};
     const Map<String, dynamic>? _data = null;
-    final _options = _setStreamType<List<User>>(Options(
+    final _options = _setStreamType<List<UserModel>>(Options(
       method: 'GET',
       headers: _headers,
       extra: _extra,
@@ -46,10 +46,10 @@ class _UserRepository implements UserRepository {
           baseUrl,
         )));
     final _result = await _dio.fetch<List<dynamic>>(_options);
-    late List<User> _value;
+    late List<UserModel> _value;
     try {
       _value = _result.data!
-          .map((dynamic i) => User.fromJson(i as Map<String, dynamic>))
+          .map((dynamic i) => UserModel.fromJson(i as Map<String, dynamic>))
           .toList();
     } on Object catch (e, s) {
       errorLogger?.logError(e, s, _options);
