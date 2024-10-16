4407d742d0b1e52906636c27ff5fea368dd49e62
ToniVinter <tonyivinter@gmail.com>
2024-10-16 16:21:26 +0300

    added splash screen and icons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:on_stage_app/app/app.dart';
 import 'package:on_stage_app/bootstrap.dart';
@@ -7,7 +8,8 @@ import 'package:on_stage_app/bootstrap.dart';
 import 'firebase_options.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
