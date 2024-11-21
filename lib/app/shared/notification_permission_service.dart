ff130e63befac7c2d42999c144bc81d92f39834c
Beniamin SZILAGYI <beniamin.szilagyi@techstackapps.com>
2024-11-21 17:58:05 +0200

    STG-163 :: Enable and disable notifications

---
+++ lib/app/shared/notification_permission_service.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class NotificationPermissionService extends StateNotifier<bool> {
+  NotificationPermissionService() : super(false) {
+    checkNotificationPermissions();
+  }
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> checkNotificationPermissions() async {
+    final NotificationSettings settings = await _firebaseMessaging.getNotificationSettings();
+
+    state = settings.authorizationStatus == AuthorizationStatus.authorized ||
+        settings.authorizationStatus == AuthorizationStatus.provisional;
+  }
+
+  Future<bool> requestNotificationPermission() async {
+    final NotificationSettings settings = await _firebaseMessaging.requestPermission();
+
+    state = settings.authorizationStatus == AuthorizationStatus.authorized ||
+        settings.authorizationStatus == AuthorizationStatus.provisional;
+
+    return state;
+  }
+
+  Future<void> openAppSettingsFunction() async {
+    if (Platform.isAndroid) {
+      await openAppSettings();
+    } else if (Platform.isIOS) {
+      await launchUrl(Uri.parse('app-settings:'));
+    }
+  }
+}
+
+
+final notificationPermissionProvider =
+StateNotifierProvider<NotificationPermissionService, bool>((ref) {
+  return NotificationPermissionService();
+});
+
