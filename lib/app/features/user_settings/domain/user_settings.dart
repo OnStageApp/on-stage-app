efef6417e56fd5d68d641759af936420a984dab2
ToniVinter <tonyivinter@gmail.com>
2025-01-28 15:20:08 +0200

    fixes

--- lib/app/features/user_settings/domain/user_settings.dart
+++ lib/app/features/user_settings/domain/user_settings.dart
@@ -1,6 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:on_stage_app/app/features/song/domain/enums/text_size.dart';
-import 'package:on_stage_app/app/features/song/domain/models/song_view_modeUserSettings with _$UserSettings {
+import 'package:on_stage_app/app/features/song/domain/models/song_view_mode.dart';
+
+part 'user_settings.freezed.dart';
+part 'user_settings.g.dart';
+
+@Freezed()
+class UserSettings with _$UserSettings {
   const factory UserSettings({
     String? id,
     String? userId,
