0dad97664d934d8851c78d2836a1fd12f3195293
ToniVinter <tonyivinter@gmail.com>
2025-02-03 10:54:11 +0200

    fixed plans, verify if there's any productId for platform specific and fixed the multitap on try for free button

--- lib/app/features/plan/application/filtered_plans_provider.dart
+++ lib/app/features/plan/application/filtered_plans_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:on_stage_app/app/features/plan/application/plan_service.dart';
 import 'package:on_stage_app/app/features/plan/domain/plan.dart';
@@ -10,9 +12,13 @@ final filteredPlansProvider = Provider<List<Plan>>((ref) {
 
   final excludedEntitlements = {'pro', 'solo', 'ultimate'};
 
-  return plans
-      .where((plan) =>
-          !excludedEntitlements.contains(plan.entitlementId) &&
-          (plan.entitlementId == 'starter' || plan.isYearly == isYearlyPlan))
-      .toList();
+  return plans.where((plan) {
+    final hasValidProductId = Platform.isAndroid
+        ? plan.googleProductId.isNotEmpty
+        : plan.appleProductId.isNotEmpty;
+
+    return hasValidProductId &&
+        !excludedEntitlements.contains(plan.entitlementId) &&
+        (plan.entitlementId == 'starter' || plan.isYearly == isYearlyPlan);
+  }).toList();
 });
