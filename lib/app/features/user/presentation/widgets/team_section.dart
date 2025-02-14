ef06a357e802d974e0090c4c2806fb3b010ea451
ToniVinter <tonyivinter@gmail.com>
2025-02-14 10:59:20 +0200

    rebased

--- lib/app/features/user/presentation/widgets/team_section.dart
+++ lib/app/features/user/presentation/widgets/team_section.dart
@@ -4,14 +4,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:on_stage_app/app/features/event/presentation/widgets/participants_on_tile.dart';
 import 'package:on_stage_app/app/features/permission/application/permission_notifier.dart';
 import 'package:on_stage_app/app/features/song/presentation/widgets/preferences/preferences_action_tile.dart';
-import 'package:on_stage_app/app/features/subscription/presentation/paywall_modal.dart';
-import 'package:on_stage_app/app/features/team/application/team_notifier.dart';
-import 'package:on_stage_app/app/features/user/domain/enums/permission_type.dart';
-import 'package:on_stage_app/app/features/user/presentation/widgets/create_new_team_button.dart';
-import 'package:on_stage_app/app/features/user/presentation/widgets/switch_teams_button.dart';
-import 'package:on_stage_app/app/router/app_router.dart';
-import 'package:on_stage_app/app/utils/build_context_extensions.dart';
-import 'package:shimmer/shimmer.dart';
+import 'package:on_stage_app/app/features/team/application/team_notifier
 
 class TeamsSection extends ConsumerWidget {
   const TeamsSection({super.key});
@@ -25,46 +18,43 @@ class TeamsSection extends ConsumerWidget {
       children: [
         const SwitchTeamsButton(),
         const SizedBox(height: 12),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 12),
-          child: ListTile(
-            splashColor: context.colorScheme.surfaceBright,
-            tileColor: context.colorScheme.onSurfaceVariant,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 12,
-            ),
-            title: (currentTeam != null)
-                ? Text(
-                    currentTeam.name,
-                    style: context.textTheme.headlineMedium,
-                  )
-                : //shimmer
-                _buildShimmer(context, height: 18, width: 200),
-            subtitle: (currentTeam != null)
-                ? Text(
-                    (currentTeam.membersCount ?? 0) > 1
-                        ? '${currentTeam.membersCount} Members'
-                        : '${currentTeam.membersCount} Member',
-                    style: context.textTheme.bodyMedium!.copyWith(
-                      color: context.colorScheme.outline,
-                    ),
-                  )
-                : _buildShimmer(context, height: 14),
-            trailing: (currentTeam != null)
-                ? ParticipantsOnTile(
-                    participantsProfileBytes: currentTeam.memberPhotos,
-                    participantsLength: currentTeam.membersCount,
-                  )
-                : const SizedBox(),
-            onTap: () {
-              context.pushNamed(
-                AppRoute.teamDetails.name,
-              );
-            },
+        ListTile(
+          splashColor: context.colorScheme.surfaceBright,
+          tileColor: context.colorScheme.onSurfaceVariant,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 12,
+          ),
+          title: (currentTeam != null)
+              ? Text(
+                  currentTeam.name,
+                  style: context.textTheme.headlineMedium,
+                )
+              : //shimmer
+              _buildShimmer(context, height: 18, width: 200),
+          subtitle: (currentTeam != null)
+              ? Text(
+                  (currentTeam.membersCount ?? 0) > 1
+                      ? '${currentTeam.membersCount} Members'
+                      : '${currentTeam.membersCount} Member',
+                  style: context.textTheme.bodyMedium!.copyWith(
+                    color: context.colorScheme.outline,
+                  ),
+                )
+              : _buildShimmer(context, height: 14),
+          trailing: (currentTeam != null)
+              ? ParticipantsOnTile(
+                  participantsProfileBytes: currentTeam.memberPhotos,
+                  participantsLength: currentTeam.membersCount,
+                )
+              : const SizedBox(),
+          onTap: () {
+            context.pushNamed(
+              AppRoute.teamDetails.name,
+            );
+          },
         ),
         if (ref.watch(permissionServiceProvider).hasAccessToEdit) ...[
           PreferencesActionTile(
@@ -81,17 +71,18 @@ class TeamsSection extends ConsumerWidget {
             },
           ),
         ] else if (!ref.watch(permissionServiceProvider).isLeaderOnTeam) ...[
-          CreateNewTeamButton(
-            icon: Icons.group,
-            title: 'Create Your Own Team',
-            actionTitle: 'Create',
-            onTap: () {
-              PaywallModal.show(
-                context: context,
-                permissionType: PermissionType.addTeamMembers,
-              );
-            },
-          ),
+          /// Disabled for now
+          // CreateNewTeamButton(
+          //   icon: Icons.group,
+          //   title: 'Create Your Own Team',
+          //   actionTitle: 'Create',
+          //   onTap: () {
+          //     PaywallModal.show(
+          //       context: context,
+          //       permissionType: PermissionType.addTeamMembers,
+          //     );
+          //   },
+          // ),
         ],
       ],
     );
---
+++ lib/l10n/arb/app_localizations_ro.dart
@@ -0,0 +1,14 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Romanian Moldavian Moldovan (`ro`).
+class AppLocalizationsRo extends AppLocalizations {
+  AppLocalizationsRo([String locale = 'ro']) : super(locale);
+
+  @override
+  String get counterAppBarTitle => 'Incrementare';
+}
